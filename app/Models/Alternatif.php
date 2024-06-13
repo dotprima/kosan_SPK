acc191abaa86fdb1d9b34b1397827c59ddf9a6c5
Your Name <you@example.com>
2024-06-13 21:49:10 +0700

    update

--- app/Models/Alternatif.php
+++ app/Models/Alternatif.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Alternatif extends Model
 {
     use HasFactory;
-    protected $fillable = ['nama','C1','C2','C3','C4','C5','C6','C7','C8','C9','kriteria_id'];
+    protected $fillable = ['kosan_id','C1','C2','C3','C4','C5','C6','C7','C8','C9','kriteria_id'];
 
     public function kriteria(){
         return $this->belongsTo(Kriteria::class, 'kriteria_id', 'id');
